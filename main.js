e4f3aaeec23176c2d65be9f73d9511621f069f36
islom709 <rislom709@gmail.com>
2025-05-21 22:58:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let son1 = +prompt('RANDOM GAMEGA XUSH KELIBSIZ  SON KIRGIZING')
+let son2 = +prompt(' IKKINCHI SONI KIRGIZING')
+let math = Math.round(Math.random() * 100)
+if ( son1 === math ||son2 === math ) {
+    alert( ' SIZ YUTINGIZ' +  math)
+} else {
+    alert('YANA BIR BOR URUNIB KORING' +  math )
+}
+inc
